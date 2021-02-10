b7eacd488038b84a7c5d0137c87f9fdd1a8a7d83
joaco <joacofracchia@gmail.com>
2021-02-10 20:29:58 -0300

    added endpoint for shorten url

--- server/config.ts
+++ server/config.ts
@@ -1,3 +1,3 @@
 module.exports = {
-  MONGO_URI: 'mongodb+srv://joacof98:fuckmoash420@cluster0.szo8u.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  MONGO_URI: 'yoururihere'
 }
--- server/routes/urls.ts
+++ server/routes/urls.ts
@@ -1,13 +1,30 @@
-const Url = require("../models/Url");
 import express from 'express';
+const Url = require("../models/Url");
+const validUrl = require('valid-url'); 
+import { nanoid } from 'nanoid'
+
 let router = express.Router()
 
-router.get("/", (req, res) => {
-  res.send("acortando")
-})
+router.post("/", async (req, res) => {
+  const { urlToShorten } = req.body;
+  if (typeof urlToShorten !== 'string' || !validUrl.isUri(urlToShorten)) {
+    res.status(400).send("Please, provide a valid Url. ⚔️")
+  } else {
+    const urlCode: string = nanoid(7)
+    const code = await Url.find({urlCode})
+    if(code.length > 0) return res.send(code.realUrl)
+    
+    const shortUrl: string = `knt.st/${urlCode}`
+    const urlShortened = new Url({
+      urlCode,
+      realUrl: urlToShorten,
+      shortUrl,
+      date: new Date().toISOString()
+    })
 
-router.post("/", (req,res) => {
-  
-})
+    await urlShortened.save()
+    return res.send(shortUrl)
+  }
+});
 
 module.exports = router
